e6f640008a83fcdb0d4b440d4c48b977c6697081
abdelrahman3003 <abdelrahmantemsah30@gmail.com>
2024-01-20 21:28:05 +0200

    link chatapp with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
